3a7e2fbf847fcd7ac8d81fc95f54d7b1e414b05e
William DeMeo <williamdemeo@gmail.com>
2019-09-22 15:17:02 -0400

    try to fix failed build

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ author = u'William DeMeo and Siva Somayyajula'
 # The short X.Y version.
 version = u'0'
 # The full version, including alpha/beta/rc tags
-release = u'0.1'
+release = u'0.2'
 
 
 # -- General configuration ---------------------------------------------------
