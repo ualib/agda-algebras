8fad594d452fdda26d002176e0647bf63994c998
William DeMeo <williamdemeo@gmail.com>
2019-10-30 16:44:18 -0400

    minor mod

--- conf.py
+++ conf.py
@@ -143,7 +143,7 @@ html_theme_options = {
     'head_font_family': 'Times New Roman, Times, serif',
     'code_bg': 'white',
     'extra_nav_links': {
-        'PDF version': 'ualib.pdf',
+        'PDF version (coming soon)': 'ualib.pdf',
         'lean-ualib gitlab repository': 'https://gitlab.com/ualib/lean-ualib',
         'logicalfoundations.org': 'https://logicalfoundations.gitlab.io/'
     },
