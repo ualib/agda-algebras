7efb25edd5f74f623a0919af1b837df9c436b7f1
William DeMeo <williamdemeo@gmail.com>
2020-03-06 11:11:11 +0100

    adding stereotype b favicon

--- conf.py
+++ conf.py
@@ -148,7 +148,7 @@ html_theme_options = {
 html_static_path = ['_static']
 
 html_logo = '_static/lambda.jpg'
-html_favicon = '_static/lambda.jpg'
+html_favicon = '_static/favicon.jpg'
 html_show_sourcelink = False
 #html_output_encoding = 'ascii'
 
